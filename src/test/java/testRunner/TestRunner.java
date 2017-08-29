d2a9b430b2969687f84ce82e51ab37dd2115bd5c
Sumit Ghosh <sumit_4ever@rediffmail.com>
2017-08-30 01:55:07 +0530

    Update TestRunner.java

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = { "src/test/resources/featureFiles" },
+		features = { "." },
 		glue = { "webDriver", "stepDefinition", "testRunner" },
 		tags = { "~@ignore"},
 		plugin = {"pretty", "html:target/cucumber-reports",
